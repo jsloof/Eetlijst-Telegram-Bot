c88ca838de025dab41f4b8e29ed1d767860b9806
Jesse Ruiter <jesse.ruiter@hotmail.com>
2020-11-27 14:03:25 +0100

    Better error handling

--- bot.py
+++ bot.py
@@ -1,11 +1,14 @@
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from telegram import ChatAction, ParseMode
-import os, logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.error import TelegramError, Unauthorized, BadRequest, TimedOut, ChatMigrated, NetworkError
+from telegram.utils.helpers import mention_html
+import logging, os, sys, traceback
 
 from bs4 import BeautifulSoup
 import requests, re
 
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
+TELEGRAM_DEV_ID = os.environ['TELEGRAM_DEV_ID']
 
 updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
 dispatcher = updater.dispatcher
@@ -14,6 +17,40 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def start(update, context):
     update.message.reply_text(f'Hallo {update.effective_user.first_name}')
+
+def error_callback(update, context):
+    # we want to notify the user of this problem. This will always work, but not notify users if the update is an
+    # callback or inline query, or a poll update. In case you want this, keep in mind that sending the message
+    # could fail
+    if update.effective_message:
+        text = "Hey. I'm sorry to inform you that an error happened while I tried to handle your update. " \
+               "My developer(s) will be notified."
+        update.effective_message.reply_text(text)
+    # This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
+    # third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
+    # for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
+    # empty string works fine.
+    trace = "".join(traceback.format_tb(sys.exc_info()[2]))
+    # lets try to get as much information from the telegram update as possible
+    payload = ""
+    # normally, we always have an user. If not, its either a channel or a poll update.
+    if update.effective_user:
+        payload += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
+    # there are more situations when you don't get a chat
+    if update.effective_chat:
+        payload += f' within the chat <i>{update.effective_chat.title}</i>'
+        if update.effective_chat.username:
+            payload += f' (@{update.effective_chat.username})'
+    # but only one where you have an empty payload by now: A poll (buuuh)
+    if update.poll:
+        payload += f' with the poll id {update.poll.id}.'
+    # lets put this in a "well" formatted text
+    text = f"Hey.\n The error <code>{context.error}</code> happened{payload}. The full traceback:\n\n<code>{trace}" \
+           f"</code>"
+    # and send it to the dev
+    context.bot.send_message(TELEGRAM_DEV_ID, text, parse_mode=ParseMode.HTML)
+    # we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
+    raise
 
 def eetlijst(update, context):
     bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
@@ -191,6 +228,7 @@ verhouding_handler = CommandHandler('verhouding', verhouding)
 schreeuw_handler = MessageHandler(Filters.text & (~Filters.command), schreeuw)
 unknown_handler = MessageHandler(Filters.command, unknown)
 
+dispatcher.add_error_handler(error_callback)
 dispatcher.add_handler(start_handler)
 dispatcher.add_handler(eetlijst_handler)
 dispatcher.add_handler(kok_handler)
