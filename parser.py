519db9155bb713418d5a608879f728d357f64fb4
Jesse Ruiter <jesse.ruiter@hotmail.com>
2020-11-29 19:55:01 +0100

    Individual replies now

--- parser.py
+++ parser.py
@@ -4,27 +4,25 @@ import os, re, requests
 class Parser:
 
     def __init__(self):
-        self.EETLIJST_USER = os.environ['EETLIJST_USER']
-        self.EETLIJST_PASS = os.environ['EETLIJST_PASS']
+        eetlijst_user = os.environ['EETLIJST_USER']
+        eetlijst_pass = os.environ['EETLIJST_PASS']
 
-        self.login_url = 'http://eetlijst.nl/login.php'
-        self.values = {'login': self.EETLIJST_USER, 'pass': self.EETLIJST_PASS}
+        login_url = 'http://eetlijst.nl/login.php'
+        login_data = {'login': eetlijst_user, 'pass': eetlijst_pass}
+        main_page = requests.post(login_url, data=login_data)
+        self.soup_main_page = BeautifulSoup(main_page.content, "html.parser")
 
-        self.main_page = requests.post(self.login_url, data=self.values)
-        self.soup_main_page = BeautifulSoup(self.main_page.content, "html.parser")
+        kosten_url = self.soup_main_page.find_all('a')[2]['href']
+        kosten_page = requests.get('http://eetlijst.nl/' + kosten_url)
+        self.soup_kosten_page = BeautifulSoup(kosten_page.content, "html.parser")
 
-        self.kosten_url = self.soup_main_page.find_all('a')[2]['href']
-        self.session_id = re.split('\W', self.kosten_url)[-1]
+        self.eetlijst = self.get_eetlijst()
+        self.persons = self.get_persons()
 
-        self.kosten_page = requests.get('http://eetlijst.nl/' + self.kosten_url)
-        self.soup_kosten_site = BeautifulSoup(self.kosten_page.content, "html.parser")
-
-        self.today_status = self.soup_main_page.find_all('td', class_='r')
-
-    # Get the eetlijst of today
+    # Returns the eetlijst of today
     def get_eetlijst(self):
         eaters, cook, absent, unknown = [], [], [], []
-        list_images = self.today_status[1].parent.find_all('img')
+        list_images = self.soup_main_page.find_all('td', class_='r')[1].parent.find_all('img')
         for image in list_images:
             choice = image['src']
             person = re.split('\s', image['title'])[0]
@@ -35,57 +33,58 @@ class Parser:
             elif choice == 'nop.gif':
                 absent.append(person)
             else:
-                person = self.get_names(len(set(eaters + cook + absent + unknown)))
+                person = self.persons.items()[len(set(eaters + cook + absent + unknown))][1]
                 unknown.append(person)
         return eaters, cook, absent, unknown
 
-    # Returns an array with the eaters
+    # Returns a list with the eaters
     def get_eaters(self):
-        return self.get_eetlijst()[0]
+        return self.eetlijst[0]
 
-    # Returns an array with the cook(s)
+    # Returns a list with the cook(s)
     def get_cook(self):
-        return self.get_eetlijst()[1]
+        return self.eetlijst[1]
 
-    # Returns an array with the absent persons
+    # Returns a list with the absent persons
     def get_absent(self):
-        return self.get_eetlijst()[2]
+        return self.eetlijst[2]
 
-    # Returns an array with the persons with unknown status
+    # Returns a list with the persons with unknown status
     def get_unknown(self):
-        return self.get_eetlijst()[3]
+        return self.eetlijst[3]
 
-    # Get the ratio cook/eat
-    def get_ratio(self, person: int = None):
+    # Returns a list with the ratio cook/eat per person
+    def get_ratio(self):
         list_ratio = []
-        all_times_cook = self.soup_kosten_site.find('td', text='  Aantal keer gekookt').parent.find_all('td', class_="r")
-        all_times_eat = self.soup_kosten_site.find('td', text='  Aantal keer meegegeten').parent.find_all('td', class_="r")
+        all_times_cook = self.soup_kosten_page.find('td', text='  Aantal keer gekookt').parent.find_all('td', class_="r")
+        all_times_eat = self.soup_kosten_page.find('td', text='  Aantal keer meegegeten').parent.find_all('td', class_="r")
         for i in range(len(all_times_cook)):
             times_cook = int(all_times_cook[i].text)
             times_eat = int(all_times_eat[i].text)
             list_ratio.append(round(times_cook / times_eat, 3))
-        return list_ratio if person == None else list_ratio[person]
+        return list_ratio
 
-    # Get the costs
-    def get_costs(self, person: int = None):
+    # Returns a list with the average meal costs per person
+    def get_costs(self):
         list_costs = []
-        all_costs = self.soup_kosten_site.find('td', text='  Kookt gemiddeld voor (p.p.)').parent.find_all('td', class_="r")[0:-1]
+        all_costs = self.soup_kosten_page.find('td', text='  Kookt gemiddeld voor (p.p.)').parent.find_all('td', class_="r")[0:-1]
         for i in range(len(all_costs)):
             list_costs.append(all_costs[i].text.strip())
-        return list_costs if person == None else list_costs[person]
+        return list_costs
 
-    # Get the names
-    def get_names(self, person: int = None):
-        list_names = []
-        all_names = self.soup_kosten_site.find('th', colspan='3').parent.find_all('th')[1:-1]
-        for i in range(len(all_names)):
-            list_names.append(all_names[i].text.strip())
-        return list_names if person == None else list_names[person]
-
-    #Get the cooking points
-    def get_points(self, person: int = None):
+    # Returns a list with the cooking points per person
+    def get_points(self):
         list_points = []
-        all_points = self.soup_kosten_site.find_all('td', class_="l", colspan='3')[-1].parent.find_all('td', class_="r")[0:-1]
+        all_points = self.soup_kosten_page.find_all('td', class_="l", colspan='3')[-1].parent.find_all('td', class_="r")[0:-1]
         for i in range(len(all_points)):
             list_points.append(all_points[i].text.strip())
-        return list_points if person == None else list_points[person]
+        return list_points
+
+    # Returns a dict with the names and telegram_ids of the persons
+    def get_persons(self, name: str = None):
+        persons = {}
+        all_names = self.soup_kosten_page.find('th', colspan='3').parent.find_all('th')[1:-1]
+        for name in all_names:
+            stripped_name = name.text.strip()
+            persons.update({stripped_name: os.environ[stripped_name]})
+        return persons if name == None else name, persons.get(name)
--- replies.py
+++ replies.py
@@ -17,13 +17,16 @@ def eetlijst():
         reply += ' en'.join(str(unknown).replace('[','').replace('\'','').replace(']','').rsplit(',', 1))
         reply += " moet" if len(unknown) == 1 else " moeten"
         reply += " zich nog inschrijven.\n"
-    return reply
+    unknown_persons = []
+    for name in unknown:
+        unknown_persons.append(ps.get_persons(name))
+    return {'reply': reply, 'unknown_persons': unknown_persons}
 
 def kok():
     ps = Parser()
     cook = ps.get_cook()
     if len(cook) == 0:
-        zipped = str(dict(zip(ps.get_ratio(), ps.get_names())))
+        zipped = str(dict(zip(ps.get_ratio(), ps.get_persons().keys())))
         reply = "Er gaat nog niemand koken, maar dit is de verhouding koken/eten:\n"
         reply += zipped.replace('{','<code>').replace(': \'','</code> (').replace('\', ',')\n<code>').replace('\'}',')')
     else:
@@ -34,18 +37,18 @@ def kok():
 
 def kookpunten():
     ps = Parser()
-    zipped = str(dict(zip(ps.get_points(), ps.get_names())))
+    zipped = str(dict(zip(ps.get_points(), ps.get_persons().keys())))
     reply = zipped.replace('{\'','<b>Kookpunten:</b>\n<code>').replace('\': \'','</code> (').replace('\', \'',')\n<code>').replace('\'}',')')
     return reply
 
 def kosten():
     ps = Parser()
-    zipped = str(dict(zip(ps.get_costs(), ps.get_names())))
+    zipped = str(dict(zip(ps.get_costs(), ps.get_persons().keys())))
     reply = zipped.replace('{\'','<b>Gemiddelde kosten:</b>\n<code>€').replace('\': \'','</code> (').replace('\', \'',')\n<code>€').replace('\'}',')')
     return reply
 
 def verhouding():
     ps = Parser()
-    zipped = str(dict(zip(ps.get_ratio(), ps.get_names())))
+    zipped = str(dict(zip(ps.get_ratio(), ps.get_persons().keys())))
     reply = zipped.replace('{','<b>Verhouding koken/eten:</b>\n<code>').replace(': \'','</code> (').replace('\', ',')\n<code>').replace('\'}',')')
     return reply
