8769396f802c067fe91c72a30243ce576df6fd65
Jesse Ruiter <jesse.ruiter@hotmail.com>
2022-08-07 16:03:48 +0200

    New error handler

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 from datetime import time
-from logging import basicConfig, INFO
+from html import escape
+from json import dumps
+from logging import basicConfig, getLogger, INFO
 from os import getenv
 from pytz import timezone
 from re import compile, search, IGNORECASE
 from replies import *
-from sys import exc_info
 from telegram import ChatAction, ParseMode, ReplyKeyboardMarkup, Update
 from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater, Dispatcher, JobQueue
-from telegram.utils.helpers import mention_html
-from traceback import format_tb
+from traceback import format_exception
 
 TELEGRAM_TOKEN = getenv('TELEGRAM_TOKEN')
 if TELEGRAM_TOKEN is None:
@@ -23,43 +23,30 @@ dispatcher: Dispatcher = updater.dispatcher
 job_queue: JobQueue = updater.job_queue
 
 basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=INFO)
+logger = getLogger(__name__)
 
 
-def error_callback(update: Update, context: CallbackContext):
-    """The callback function for erroneous commands."""
-    # We want to notify the user of this problem. This will always work,
-    # but not notify users if the update is callback or inline query, or a poll update.
-    # In case you want this, keep in mind that sending the message could fail.
-    if update.effective_message:
-        text = 'Hallo. Er is helaas een fout opgetreden bij het verwerken van uw verzoek. ' \
-               'Mijn ontwikkelaar wordt op de hoogte gesteld.'
-        update.effective_message.reply_text(text)
+def error_callback(update: object, context: CallbackContext):
+    """Log the error and send a telegram message to notify the developer."""
+    logger.error('Exception while handling an update:', exc_info=context.error)
+    if isinstance(update, Update):
+        if update.effective_message:
+            text = ('Hallo. Er is helaas een fout opgetreden bij het verwerken van uw verzoek. '
+                    'Mijn ontwikkelaar wordt op de hoogte gesteld.')
+            update.effective_message.reply_text(text)
+        update_str = update.to_dict()
+    else:
+        update_str = str(update)
     if TELEGRAM_DEV_ID is None:
         return
-    # This traceback is created with accessing the traceback object from the sys.exc_info, which is returned as the
-    # third value of the returned tuple. Then we use the traceback.format_tb to get the traceback as a string, which
-    # for a weird reason separates the line breaks in a list, but keeps the linebreaks itself. So just joining an
-    # empty string works fine.
-    trace = ''.join(format_tb(exc_info()[2]))
-    # let's try to get as much information from the telegram update as possible
-    info = ''
-    # normally, we always have a user. If not, it's either a channel or a poll update.
-    if update.effective_user:
-        info += f' with the user {mention_html(update.effective_user.id, update.effective_user.first_name)}'
-    # there are more situations when you don't get a chat
-    if update.effective_chat:
-        info += f' within the chat <i>{update.effective_chat.title}</i>'
-        if update.effective_chat.username:
-            info += f' (@{update.effective_chat.username})'
-    # but only one where you have an empty payload by now: a poll
-    if update.poll:
-        info += f' with the poll id {update.poll.id}'
-    # let's put this in a "well" formatted text
-    text = f'Hey.\nThe error <code>{context.error}</code> happened{info}. The full traceback:\n\n<code>{trace}</code>'
-    # and send it to the dev
+    tb_list = format_exception(None, context.error, context.error.__traceback__)
+    tb_string = ''.join(tb_list)
+    text = (f'An exception was raised while handling an update\n'
+            f'<pre>update = {escape(dumps(update_str, ensure_ascii=False, indent=2))}</pre>\n\n'
+            f'<pre>context.chat_data = {escape(str(context.chat_data))}</pre>\n\n'
+            f'<pre>context.user_data = {escape(str(context.user_data))}</pre>\n\n'
+            f'<pre>{escape(tb_string)}</pre>')
     context.bot.send_message(TELEGRAM_DEV_ID, text, ParseMode.HTML)
-    # we raise the error again, so the logger module catches it. If you don't use the logger module, use it.
-    raise
 
 
 def start_callback(update: Update, context: CallbackContext):
@@ -90,10 +77,10 @@ def individual_callback(context: CallbackContext):
     user_id = person.get_user_id()
     if user_id is not None:
         context.bot.send_chat_action(user_id, ChatAction.TYPING)
+        text = f'{person}, je moet je nog inschrijven voor de maaltijd!\nJe kunt reageren in deze chat.'
         custom_keyboard = [['Ik eet mee🍔'], ['Ik eet niet mee🙅'], ['Ik kook🧑‍🍳']]
         reply_markup = ReplyKeyboardMarkup(custom_keyboard)
-        context.bot.send_message(user_id, f'{person}, je moet je nog inschrijven voor de maaltijd!\n' +
-                                 'Je kunt reageren in deze chat.', ParseMode.HTML, reply_markup=reply_markup)
+        context.bot.send_message(user_id, text, ParseMode.HTML, reply_markup=reply_markup)
 
 
 def kok_callback(update: Update, context: CallbackContext):
